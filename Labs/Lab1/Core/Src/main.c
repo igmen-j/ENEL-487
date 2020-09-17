a0a2c9024d8428cebdc0a8b63999b293375bfeb8
justinigmen <igmenjustin@gmail.com>
2020-09-16 19:10:35 -0600

    Lab 1 update - complete

--- Labs/Lab1/Core/Src/main.c
+++ Labs/Lab1/Core/Src/main.c
@@ -1,4 +1,12 @@
 /* USER CODE BEGIN Header */
+
+/**
+ * Justin Igmen
+ * 200364880
+ *  Lab 1
+ */
+
+
 /**
   ******************************************************************************
   * @file           : main.c
@@ -40,6 +48,7 @@
 /* USER CODE END PM */
 
 /* Private variables ---------------------------------------------------------*/
+TIM_HandleTypeDef htim4;
 
 /* USER CODE BEGIN PV */
 
@@ -48,6 +57,7 @@
 /* Private function prototypes -----------------------------------------------*/
 void SystemClock_Config(void);
 static void MX_GPIO_Init(void);
+static void MX_TIM4_Init(void);
 /* USER CODE BEGIN PFP */
 
 /* USER CODE END PFP */
@@ -85,7 +95,13 @@ int main(void)
 
   /* Initialize all configured peripherals */
   MX_GPIO_Init();
+  MX_TIM4_Init();
   /* USER CODE BEGIN 2 */
+
+  uint16_t timer;
+
+  HAL_TIM_Base_Start(&htim4);	//tim4 starts counting
+  timer = __HAL_TIM_GET_COUNTER(&htim4);	//gets the counter value
 
   /* USER CODE END 2 */
 
@@ -96,8 +112,20 @@ int main(void)
     /* USER CODE END WHILE */
 
     /* USER CODE BEGIN 3 */
-	  HAL_GPIO_TogglePin(GPIOA, GPIO_PIN_5);
-	  HAL_Delay(1000);
+	  /* prescaler is set to 8000
+	   * clock speed = 8MHz
+	   * 8MHz/8000 = 1tick/0.001
+	   * For 1sec, we need 1000 ticks
+	   */
+
+	  if (__HAL_TIM_GET_COUNTER(&htim4) - timer >= 1000)	//time elapsed between prev count to current
+	  {
+		  HAL_GPIO_TogglePin(GPIOA, GPIO_PIN_5);	//toggle pin if 1sec has passed
+		  timer = __HAL_TIM_GET_COUNTER(&htim4);	//get new count
+	  }
+
+	  //HAL_Delay(1000);
+
   }
   /* USER CODE END 3 */
 }
@@ -135,6 +163,51 @@ void SystemClock_Config(void)
   {
     Error_Handler();
   }
+}
+
+/**
+  * @brief TIM4 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_TIM4_Init(void)
+{
+
+  /* USER CODE BEGIN TIM4_Init 0 */
+
+  /* USER CODE END TIM4_Init 0 */
+
+  TIM_ClockConfigTypeDef sClockSourceConfig = {0};
+  TIM_MasterConfigTypeDef sMasterConfig = {0};
+
+  /* USER CODE BEGIN TIM4_Init 1 */
+
+  /* USER CODE END TIM4_Init 1 */
+  htim4.Instance = TIM4;
+  htim4.Init.Prescaler = 8000;
+  htim4.Init.CounterMode = TIM_COUNTERMODE_UP;
+  htim4.Init.Period = 65535;
+  htim4.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
+  htim4.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_DISABLE;
+  if (HAL_TIM_Base_Init(&htim4) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  sClockSourceConfig.ClockSource = TIM_CLOCKSOURCE_INTERNAL;
+  if (HAL_TIM_ConfigClockSource(&htim4, &sClockSourceConfig) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  sMasterConfig.MasterOutputTrigger = TIM_TRGO_RESET;
+  sMasterConfig.MasterSlaveMode = TIM_MASTERSLAVEMODE_DISABLE;
+  if (HAL_TIMEx_MasterConfigSynchronization(&htim4, &sMasterConfig) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN TIM4_Init 2 */
+
+  /* USER CODE END TIM4_Init 2 */
+
 }
 
 /**
