31d22621ffdacf6c0df4fdd883586b5371d26d69
justinigmen <igmenjustin@gmail.com>
2020-10-17 15:22:44 -0600

    Added error checks -Added error check for division by zero -Added error check when user enters too many inputs for the command -Added error when input is invalid

--- Assignments/A3/A3.cpp
+++ Assignments/A3/A3.cpp
@@ -26,6 +26,8 @@ using namespace std;
 
 #define LINE_BUFFER 200
 #define PI 3.14159265359
+#define BASIC_MATH_INPUT 5
+#define COMPLEX_MATH_INPUT 3
 
 //Data type for the complex number
 class Complex {
@@ -37,22 +39,25 @@ public:
 Complex add(Complex, Complex);
 Complex sub(Complex, Complex);
 Complex mul(Complex, Complex);
-Complex div(Complex, Complex);
+Complex div(Complex, Complex, bool&);
 double abs(Complex);
-double arg(Complex);
-double argDeg(Complex);
+double arg(Complex, bool&);
+double argDeg(Complex, bool&);
 Complex exp(Complex);
-Complex inv(Complex);
-Complex solve(char[], Complex, Complex, bool&, int);
+Complex inv(Complex, bool&);
+Complex solve(char[], Complex, Complex, bool&, bool&, int);
 void print_result(char[], Complex);
 bool user_quit(char[]);
+bool is_basic_math(char[]);
+bool is_complex_math(char[]);
 
 
 int main() {
     Complex z1, z2, answer;
-    char user_input[LINE_BUFFER], command[LINE_BUFFER];
+    char user_input[LINE_BUFFER], command[LINE_BUFFER], input_overflow[LINE_BUFFER];
     int check_interactive;
     bool is_error;
+    bool div_by_zero;
     bool is_done = false;
 
     fprintf(stderr, "Complex calculator\n\n");
@@ -63,10 +68,12 @@ int main() {
     //Will run until user enters "Q" in the command
     while (!is_done) {
         is_error = false;
+        div_by_zero = false;
 
         fprintf(stderr, "Enter exp: ");
         fgets(user_input, sizeof(user_input), stdin);
-        check_interactive = sscanf(user_input, "%s %lf %lf %lf %lf\n", command, &z1.real, &z1.imag, &z2.real, &z2.imag);
+        check_interactive = sscanf(user_input, "%s %lf %lf %lf %lf %s\n", command, &z1.real, &z1.imag, 
+            &z2.real, &z2.imag, input_overflow);
 
         if (user_quit(user_input)) {
             is_done = true;
@@ -76,17 +83,38 @@ int main() {
         }
         else {
             //Checks if input is entered properly
-            if (check_interactive == 3 || check_interactive == 5) {
-                answer = solve(command, z1, z2, is_error, check_interactive);
+            if (check_interactive == COMPLEX_MATH_INPUT || check_interactive == BASIC_MATH_INPUT) {
+                answer = solve(command, z1, z2, is_error, div_by_zero, check_interactive);
                 if (is_error == true) {
-                    fprintf(stderr, "Invalid operator.\n");
+                    if (is_basic_math(command)) {
+                        fprintf(stderr, "Error: Required input for this operator is 5.\n");
+                    }
+                    else if (is_complex_math(command)) {
+                        fprintf(stderr, "Error: Required input for this operator is 3.\n");
+                    }
+                    else {
+                        fprintf(stderr, "Error: Invalid operator.\n");
+                    }
                 }
                 else {
-                    print_result(command, answer);
+                    if (div_by_zero == true) {
+                        fprintf(stderr, "Error: Division by zero.\n");
+                    }
+                    else {
+                        print_result(command, answer);
+                    }
                 }
             }
             else {
-                fprintf(stderr, "Invalid input.\n");
+                if (check_interactive > BASIC_MATH_INPUT && is_basic_math(command)) {
+                    fprintf(stderr, "Error: Required input for this operator is 5.\n");
+                }
+                else if (check_interactive > COMPLEX_MATH_INPUT && is_complex_math(command)) {
+                    fprintf(stderr, "Error: Required input for this operator is 3.\n");
+                }
+                else {
+                    fprintf(stderr, "Error: Invalid input.\n");
+                }
             }
         }
     }
@@ -97,23 +125,18 @@ int main() {
 Prints the answer and formats it
 */
 void print_result(char command[], Complex answer) {
-    //Checks if divided by zero
-    if (isnan(answer.real) && isnan(answer.imag)) {
-        fprintf(stdout, "nan + j nan\n");
+
+    if (strcmp(command, "abs") == 0 || strcmp(command, "arg") == 0 || strcmp(command, "argdeg") == 0) {
+        fprintf(stdout, "%.5e\n", answer.real);
     }
     else {
-        if (strcmp(command, "abs") == 0 || strcmp(command, "arg") == 0 || strcmp(command, "argdeg") == 0) {
-            fprintf(stdout, "%.5e\n", answer.real);
+        //Checks if imaginary component of the answer is negative and formats the answer accordingly
+        if (answer.imag < 0) {
+            answer.imag *= -1;
+            fprintf(stdout, "%.5e - j %.5e\n", answer.real, answer.imag);
         }
         else {
-            //Checks if imaginary component of the answer is negative and formats the answer accordingly
-            if (answer.imag < 0) {
-                answer.imag *= -1;
-                fprintf(stdout, "%.5e - j %.5e\n", answer.real, answer.imag);
-            }
-            else {
-                fprintf(stdout, "%.5e + j %.5e\n", answer.real, answer.imag);
-            }
+            fprintf(stdout, "%.5e + j %.5e\n", answer.real, answer.imag);
         }
     }
 }
@@ -128,6 +151,23 @@ bool user_quit(char user_input[]) {
     }
 }
 
+bool is_basic_math(char command[]) {
+    if (strcmp(command, "a") == 0 || strcmp(command, "A") == 0 || strcmp(command, "s") == 0 || 
+        strcmp(command, "S") == 0 || strcmp(command, "m") == 0 || strcmp(command, "M") == 0 || 
+        strcmp(command, "d") == 0 || strcmp(command, "D") == 0) {
+        return true;
+    }
+    return false;
+}
+
+bool is_complex_math(char command[]) {
+    if (strcmp(command, "abs") == 0 || strcmp(command, "arg") == 0 || strcmp(command, "argdeg") == 0 ||
+        strcmp(command, "exp") == 0 || strcmp(command, "inv") == 0) {
+        return true;
+    }
+    return false;
+}
+
 /*
 Solves the command entered
 
@@ -140,14 +180,15 @@ Output:
 If an error is found (eg. sign is not a,s,m,d), bool error will be set
 Returns the answer in complex format
 */
-Complex solve(char sign[], Complex z1, Complex z2, bool& error, int num_inputs) {
+Complex solve(char sign[], Complex z1, Complex z2, bool& error, bool& div_by_zero, int num_inputs) {
     Complex answer;
     answer.real = 0;
     answer.imag = 0;
     error = false;
+    div_by_zero = false;
     
     //Checks which operation to use
-    if (num_inputs == 5) {
+    if (num_inputs == BASIC_MATH_INPUT) {
         if (strcmp(sign, "a") == 0 || strcmp(sign, "A") == 0) {
             answer = add(z1, z2);
         }
@@ -158,27 +199,27 @@ Complex solve(char sign[], Complex z1, Complex z2, bool& error, int num_inputs) 
             answer = mul(z1, z2);
         }
         else if (strcmp(sign, "d") == 0 || strcmp(sign, "D") == 0) {
-            answer = div(z1, z2);
+            answer = div(z1, z2, div_by_zero);
         }
         else {
             error = true;
         }
     }
-    else if (num_inputs == 3) {
+    else if (num_inputs == COMPLEX_MATH_INPUT) {
         if (strcmp(sign, "abs") == 0) {
             answer.real = abs(z1);
         }
         else if (strcmp(sign, "arg") == 0) {
-            answer.real = arg(z1);
+            answer.real = arg(z1, div_by_zero);
         }
         else if (strcmp(sign, "argdeg") == 0) {
-            answer.real = argDeg(z1);
+            answer.real = argDeg(z1, div_by_zero);
         }
         else if (strcmp(sign, "exp") == 0) {
             answer = exp(z1);
         }
         else if (strcmp(sign, "inv") == 0) {
-            answer = inv(z1);
+            answer = inv(z1, div_by_zero);
         }
         else {
             error = true;
@@ -216,11 +257,11 @@ Complex mul(Complex z1, Complex z2) {
 
 //Division
 //If division by zero, set answer to NAN
-Complex div(Complex z1, Complex z2) {
+Complex div(Complex z1, Complex z2, bool &div_by_zero) {
     Complex quotient;
     if (z2.real == 0 && z2.imag == 0) {
-        quotient.real = NAN;
-        quotient.imag = NAN;
+        div_by_zero = true;
+        return z2;
     }
     else {
         quotient.real = ((z1.real * z2.real) + (z1.imag * z2.imag)) / ((z2.real * z2.real) + (z2.imag * z2.imag));
@@ -235,15 +276,22 @@ double abs(Complex z1) {
 }
 
 //Returns the angle of the input complex number in radians
-double arg(Complex z1) {
+double arg(Complex z1, bool& div_by_zero) {
+    if (z1.real == 0) {
+        div_by_zero = true;
+        return z1.real;
+    }
     return atan(z1.imag / z1.real);
 }
 
 //Returns the angle of the input complex number in degrees
-double argDeg(Complex z1) {
+double argDeg(Complex z1, bool& div_by_zero) {
     double answer;
+    if (z1.real == 0) {
+        div_by_zero = true;
+        return z1.real;
+    }
     answer = atan(z1.imag / z1.real) * 180 / PI;
-
     if (z1.real < 0 && z1.imag > 0) {
         answer += 180;
     }
@@ -262,8 +310,12 @@ Complex exp(Complex z1) {
 }
 
 //Return the reciprocal of the input complex number
-Complex inv(Complex z1) {
+Complex inv(Complex z1, bool& div_by_zero) {
     Complex answer;
+    if (z1.real == 0 && z1.imag == 0) {
+        div_by_zero = true;
+        return z1;
+    }
     answer.real = z1.real / (pow(z1.real, 2) + pow(z1.imag, 2));
     answer.imag = -1 * (z1.imag / (pow(z1.real, 2) + pow(z1.imag, 2)));
     return answer;
