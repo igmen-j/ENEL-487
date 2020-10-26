26566e9eeeda3b59f49f1e58e0f94661196717d4
Justin Igmen <55956362+igmen-j@users.noreply.github.com>
2020-10-26 15:49:55 -0600

    Lab reupload

--- Labs/Lab2/Core/Startup/startup_stm32f100rbtx.s
+++ Labs/Lab2/Core/Startup/startup_stm32f100rbtx.s
@@ -61,7 +61,8 @@ defined in linker script */
   .type Reset_Handler, %function
 Reset_Handler:
 
-/* Copy the data segment initializers from flash to SRAM */  movs r1, #0
+/* Copy the data segment initializers from flash to SRAM */
+  movs r1, #0
   b LoopCopyDataInit
 
 CopyDataInit:
